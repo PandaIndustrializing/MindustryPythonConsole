1342854b1ba850dd643eca378c085f4f51deafad
PandaIndustrializing <151769754+PandaIndustrializing@users.noreply.github.com>
2023-11-23 06:44:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,13 @@ Class Player:
       'Silicon' : 0
       'Tungsten' : 0
     }
-    
+ 
+  def save(self)
+    self.data = {
+      'name' : name,
+      'resources' : resources
+    }
+  
+  def load(self)
+    with open('Database.json', 'r') as file:
+  
