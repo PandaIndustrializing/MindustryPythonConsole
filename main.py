c9084649aad4580f12fcbeec9c54f6cebbeb4e3d
PandaIndustrializing <151769754+PandaIndustrializing@users.noreply.github.com>
2023-11-23 06:38:56 +0800

    Update main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+Class Player:
+  def __init__(self, name):
+    self.name = name
+    self.resources = {
+      'Beryllium' : 140,
+      'Graphite' : 0,
+      'Sand' : 0,
+      'Silicon' : 0
+      'Tungsten' : 0
+    }
+    
